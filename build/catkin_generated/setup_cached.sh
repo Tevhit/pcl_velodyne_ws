524c60079d77a2445d489bc13f2ba56a2c10bf3f
tevhit <tevhitkarsli@hotmail.com>
2023-06-27 14:02:47 +0300

    velodyne_simulator plugin include

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tevhit/pcl_velodyne_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tevhit/pcl_velodyne_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/tevhit/pcl_velodyne_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tevhit/pcl_velodyne_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/tevhit/pcl_velodyne_ws/src:$ROS_PACKAGE_PATH"
